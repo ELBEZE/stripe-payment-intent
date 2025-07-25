f13a519c9fd7cd33afc1b2c0c889d103c9eb0526
ELBEZE <contact.studentformation@gmail.com>
2025-07-25 14:28:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Sert le fichier index.html
+app.use(express.static(path.join(__dirname, ".")));
 
 app.post("/create-payment-intent", async (req, res) => {
   const { amount } = req.body;
